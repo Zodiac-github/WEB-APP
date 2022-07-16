f6ece9adbdfb131cc75afa7883a0d818ee73e986
Zodiac-github <ethancoleman159@gmail.com>
2022-07-16 13:39:13 +1000

    Week 1 Completion

---
+++ Weekly Tasks 1/Task 5/Todo.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: "#app",
+    data: {
+        todos: [
+            { title: "Assignment1", checked: true },
+            { title: "Assignment2", checked: false },
+            { title: "Assignment3", checked: false },
+        ],
+    },
+});
